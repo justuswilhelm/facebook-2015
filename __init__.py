6a2a7ca2368ec45bb09272d65e5837da0fa7f9ba
Justus Perlwitz <mail@justusperlwitz.com>
2015-09-21 14:07:58 +0200

    Add basic user authentication and registration

--- __init__.py
+++ __init__.py
@@ -1,10 +1,60 @@
+from os import getenv
+
 from flask import (
+    flash,
     Flask,
+    redirect,
     render_template,
+    request,
+    session,
 )
+from redis import Redis
 
 application = Flask(__name__)
+application.db = Redis.from_url(
+    getenv('REDIS_URL', 'redis://localhost:6379/'))
+
+application.secret_key = 'LOL'
 
 @application.route("/")
 def index():
-    return render_template('index.html')
+    if application.db.exists(session.get('user')):
+        return render_template('index.html')
+    else:
+        return redirect("register")
+
+
+# Register
+# Log In
+@application.route("/register")
+def register_get():
+    return render_template('register.html')
+
+
+@application.route("/register", methods=["post"])
+def register_post():
+    name = request.form['name']
+    password = request.form['password']
+
+    application.db.set(name, password)
+    session['user'] = name
+    flash("Thank you for registering.")
+    return redirect('/')
+
+
+@application.route("/login")
+def login_get():
+    return render_template('login.html')
+
+
+@application.route("/login", methods=['post'])
+def login_post():
+    name = request.form['name']
+    password = request.form['password']
+
+    if application.db.get(name) == password:
+        flash("You were logged in successfully.")
+        return redirect('/')
+    else:
+        flash("You were not loggin in successfully.")
+        return redirect('/login')
